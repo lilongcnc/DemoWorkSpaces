7ae89df77dce97aa8a0ffb206660e62b54c8c033
lilongcnc <lilongcnc@vip.qq.com>
2016-03-23 21:06:02 +0800

    创建项目工程文件,此项目用于叶孤城的下载 DanceGroup 的下载部分

---
+++ DownLoadDemo/DownLoadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DownLoadDemo
+//
+//  Created by 李龙 on 16/3/23.
+//  Copyright © 2016年 lauren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DownLoadDemo/DownLoadDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DownLoadDemo
+//
+//  Created by 李龙 on 16/3/23.
+//  Copyright © 2016年 lauren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
